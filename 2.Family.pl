b1b5ec859324485f073cfff550f03cccc95a06e3
Hithesh reddy <reddylohitha774@gmail.com>
2026-02-18 08:59:32 +0530

    Add files via upload

---
+++ 2.Family.pl
@@ -0,0 +1,40 @@
+
+female(pam).
+female(liz).
+female(ann).
+female(pat).
+male(tom).
+male(bob).
+male(jim).
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(pat, liz).
+parent(bob, ann).
+parent(liz, ann).
+parent(bob, jim).
+parent(liz, jim).
+mother(X, Y) :-
+female(X),
+parent(X, Y).
+father(X, Y) :-
+male(X),
+parent(X, Y).
+grandfather(X, Y) :-
+male(X),
+parent(X, Z),
+parent(Z, Y).
+grandmother(X, Y) :-
+female(X),
+parent(X, Z),
+parent(Z, Y).
+sister(X, Y) :-
+female(X),
+parent(P, X),
+parent(P, Y),
+X \= Y.
+brother(X, Y) :-
+male(X),
+parent(P, X),
+parent(P, Y),
+X \= Y.
